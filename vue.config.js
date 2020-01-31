4b77ba856cb239f72a3149d81d67ca8fae39e6a5
3tw <55413158+3tw@users.noreply.github.com>
2020-01-31 20:15:47 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 const GoogleFontsPlugin = require("google-fonts-webpack-plugin");
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/portfolio/'
-    : '/',
+    publicPath: '',
     css: {
         loaderOptions: {
             sass: {
